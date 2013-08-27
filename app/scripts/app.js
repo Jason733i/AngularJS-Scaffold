e767431445f26c076ce6da4b98e38127f96f6b1b
Cyrus Team <cyrusteam@cyruslists.com>
2013-08-27 12:02:42 -0400

    Adding the previously generated scaffold

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('angularScaffoldApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
